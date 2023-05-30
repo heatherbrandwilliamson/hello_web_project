9d1781a4137f11174cf01de1e0ce610bc3643097
heatherbrandwilliamson <heather.brandwilliamson@gmail.com>
2023-05-30 16:08:28 +0100

    updated rspec

---
+++ app.rb
@@ -0,0 +1,67 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class Application < Sinatra::Base
+
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    return "Hello Heather"
+  end
+end
+
+
+
+  # This allows the app code to refresh
+  # without having to restart the server.
+#   post '/POST/Submit' do
+#     name = params[:name]
+#     message = params[:message]
+#     return name, message
+#     # This route is not executed (the method doesn't match).
+#   end
+
+#   get '/hello?name=Leo' do
+#     return name
+#     # This route is not executed (the path doesn't match).    
+#   end
+
+# get '/' do
+#   return "Hello Heather"
+# end
+
+#   post '/submit' do
+#     name = params[:name]
+#     message = params[:message]
+#     return "Thanks #{name}, you sent this message: #{message}"
+# #     # This route matches too, but will not be executed.
+# #     # Only the first one matching (above) is.
+#   end
+
+#   get '/posts' do
+#     name = params[:name]
+#     cohort_name = params[:cohort_name]
+#     p name
+#     p cohort_name
+#     return "hello this is a post!"
+#     # This route matches too, but will not be executed.
+#     # Only the first one matching (above) is.
+#   end
+
+
+
+  #get '/posts' do 
+  #  return 'A list of posts'
+  #end
+
+  #posts '/posts' do
+  #end 
+
+
+# GET / -> exectute some code 
+
+# GET / todo/1 -> execute some different piece of code
+
+# POST /todos -> execute some different piece of code
